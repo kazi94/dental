61c6353d4f7089e7e247759014149a21b234a3c6
kazi94 <kazi.sidou.94@gmail.com>
2020-06-30 13:31:57 +0100

    kjhkhjkhjjhkhjkhjk

--- resources/js/components/GalleryComponent.vue
+++ resources/js/components/GalleryComponent.vue
@@ -3,11 +3,7 @@
         <ul id="lightgallery" class="list-unstyled row" style="overflow-y: scroll; height: 200px">
             <li class="col-xs-6 col-sm-4 col-md-3" v-for="(radio,index) in patient.radios" style="padding-left: 0px; padding-right: 0px;" :data-src="radio.img_url" data-sub-html="text here">
                 <a href="">
-<<<<<<< HEAD
                     <img class="img-responsive" :src="radio.img_url" :alt="radio.id">
-=======
-                    <img class="img-responsive" :src="radio.img_url" :alt="radio.id" />
->>>>>>> a3433d2ecbd535b1e67896b2cf4c7a16d59556b3
                 </a>
             </li>
         </ul>
@@ -20,22 +16,14 @@
 		props : [
 			'patient',
 		],
-<<<<<<< HEAD
 		data() 
 		{
-=======
-		data() {
->>>>>>> a3433d2ecbd535b1e67896b2cf4c7a16d59556b3
 			return {
 
             };
 		},
-<<<<<<< HEAD
 		methods : 
 		{
-=======
-		methods : {
->>>>>>> a3433d2ecbd535b1e67896b2cf4c7a16d59556b3
 		},
 
 		mounted() {
