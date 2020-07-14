53a3203b33a003b6fe4bbb277c0585ed10645186
__KaziWhiteSalafiDev <kazi.sidou.94@gmail.com>
2020-07-14 21:57:25 +0100

    UPDATE APP.php

--- app.php
+++ app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'AnaPharmDz'),
+//                 HERE I ADD SOMME DISTANT COMMAND
 
     /*
     |--------------------------------------------------------------------------
